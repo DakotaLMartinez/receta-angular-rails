ff224f582c4c893a5508168d7540db6d582fa7e8
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2016-08-06 20:02:28 -0700

    uses github gist for capistrano deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,61 +1,100 @@
-# config valid only for current version of Capistrano
-lock '3.6.0'
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
 
-set :application, 'angular_rails_receta'
-set :repo_url, 'git@github.com:DakotaLMartinez/receta-angular-rails.git'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/dakotaleedev/webapps/rails_angular_receta'
+set :user, "dakotaleedev"
+set :application, "angular_rails_receta"
+set :repository,  "git@github.com:DakotaLMartinez/receta-angular-rails.git"
+set :deploy_to, "/home/dakotaleedev/webapps/angular_rails_receta"
+set :default_stage, "production"
+set :verbose_command_log, true
+set :use_sudo, false
+set :deploy_via, :checkout
+set :branch, "master"
 
 set :default_environment, {
-  'PATH' => '#{deploy_to}/bin:$PATH',
-  'GEM_HOME' => '#{deploy_to}/gems'
+  "PATH"      =>  "#{deploy_to}/bin:$PATH",
+  "GEM_HOME"  =>  "#{deploy_to}/gems",
+  "RAILS_ENV" =>  "#{default_stage}"
 }
 
-set :tmp_dir, '/home/dakotaleedev/tmp'
+set :scm, :git
+set :scm_username, "dakotaleemusic@gmail.com"
+role :web, "dakotaleedev@dakotaleedev.webfactional.com"
+role :app, "dakotaleedev@dakotaleedev.webfactional.com"
+role :db,  "dakotaleedev@dakotaleedev.webfactional.com", :primary => true
 
-namespace :deploy do 
-  desc 'Restart application'
-    task :restart do 
-      on roles(:app), in: :sequence, wait: 5 do 
-        capture("#{deploy_to}/bin/restart")
+
+
+desc "Restart nginx"
+task :restart do
+  run "#{deploy_to}/bin/restart"
+end
+
+desc "Start nginx"
+task :start do
+  run "#{deploy_to}/bin/start"
+end
+
+desc "Stop nginx"
+task :stop do
+  run "#{deploy_to}/bin/stop"
+end
+
+
+
+namespace :deploy do
+
+  puts "===================================================\n"
+  puts "         (  )   (   )  )"
+  puts "      ) (   )  (  (         GO GRAB SOME COFFEE"      
+  puts "      ( )  (    ) )\n"
+  puts "     <_____________> ___    CAPISTRANO IS ROCKING!"
+  puts "     |             |/ _ \\"
+  puts "     |               | | |"
+  puts "     |               |_| |"
+  puts "  ___|             |\\___/"
+  puts " /    \\___________/    \\"
+  puts " \\_____________________/ \n"
+  puts "==================================================="
+
+  desc "Remake database"
+  task :remakedb do
+    run "cd #{deploy_to}/current; bundle exec rake db:migrate RAILS_ENV=#{default_stage}"
+    run "cd #{deploy_to}/current; bundle exec rake db:seed RAILS_ENV=#{default_stage}"
+  end
+
+  desc "Seed database"
+  task :seed do
+    run "cd #{deploy_to}/current; bundle exec rake db:seed RAILS_ENV=#{default_stage}"
+  end
+
+  desc "Migrate database"
+  task :migrate do
+    run "cd #{deploy_to}/current; bundle exec rake db:migrate RAILS_ENV=#{default_stage}"
+  end
+
+  desc "Bundle install gems"
+  task :bundle do
+    run "cd #{deploy_to}/current; bundle install --deployment"
+  end
+
+  namespace :assets do
+    desc "Run the precompile task remotely"
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      run "cd #{deploy_to}/current; bundle exec rake assets:precompile RAILS_ENV=#{default_stage}"
     end
   end
+
 end
 
-namespace :gems do 
-  task :bundle do
-    run "cd #{release_path} && bundle install --deployment --without development test"
-  end
-end
 
-after 'deploy:publishing', 'gems:bundle'
-after 'deploy:publishing', 'deploy:restart'
 
-# Default value for :scm is :git
-# set :scm, :git
+after "deploy", "deploy:bundle"
+after "deploy", "deploy:assets:precompile"
+after "deploy", "deploy:migrate"
+after "deploy", "deploy:cleanup"
+after "deploy", "restart"
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+#
