230afef75ce77af14ec544c0ce863b373856cee5
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2016-08-06 20:14:39 -0700

    changes to capture in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,8 @@
-
-
-
 set :user, "dakotaleedev"
 set :application, "angular_rails_receta"
 set :repository,  "git@github.com:DakotaLMartinez/receta-angular-rails.git"
 set :deploy_to, "/home/dakotaleedev/webapps/angular_rails_receta"
+# set :default_stage, "production"
 set :verbose_command_log, true
 set :use_sudo, false
 set :deploy_via, :checkout
@@ -58,29 +56,29 @@ namespace :deploy do
 
   desc "Remake database"
   task :remakedb do
-    run "cd #{deploy_to}/current; bundle exec rake db:migrate RAILS_ENV=production"
-    run "cd #{deploy_to}/current; bundle exec rake db:seed RAILS_ENV=production"
+    capture("cd #{deploy_to}/current; bundle exec rake db:migrate RAILS_ENV=production")
+    capture("cd #{deploy_to}/current; bundle exec rake db:seed RAILS_ENV=production")
   end
 
   desc "Seed database"
   task :seed do
-    run "cd #{deploy_to}/current; bundle exec rake db:seed RAILS_ENV=production"
+    capture("cd #{deploy_to}/current; bundle exec rake db:seed RAILS_ENV=production")
   end
 
   desc "Migrate database"
   task :migrate do
-    run "cd #{deploy_to}/current; bundle exec rake db:migrate RAILS_ENV=production"
+    capture("cd #{deploy_to}/current; bundle exec rake db:migrate RAILS_ENV=production")
   end
 
   desc "Bundle install gems"
   task :bundle do
-    run "cd #{deploy_to}/current; bundle install --deployment"
+    capture("cd #{deploy_to}/current; bundle install --deployment")
   end
 
   namespace :assets do
     desc "Run the precompile task remotely"
     task :precompile, :roles => :web, :except => { :no_release => true } do
-      run "cd #{deploy_to}/current; bundle exec rake assets:precompile RAILS_ENV=production"
+      capture("cd #{deploy_to}/current; bundle exec rake assets:precompile RAILS_ENV=production")
     end
   end
 
@@ -89,9 +87,69 @@ end
 
 
 after "deploy", "deploy:bundle"
-# after "deploy", "deploy:assets:precompile"
-# after "deploy", "deploy:migrate"
-# after "deploy", "deploy:cleanup"
+after "deploy", "deploy:assets:precompile"
+after "deploy", "deploy:migrate"
+after "deploy", "deploy:cleanup"
 after "deploy", "restart"
 
-#
+# # config valid only for current version of Capistrano
+# lock '3.6.0'
+
+# set :application, 'angular_rails_receta'
+# set :repo_url, 'git@github.com:DakotaLMartinez/receta-angular-rails.git'
+
+# # Default branch is :master
+# # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# # Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, '/home/dakotaleedev/webapps/rails_angular_receta'
+
+# set :default_environment, {
+#   'PATH' => '#{deploy_to}/bin:$PATH',
+#   'GEM_HOME' => '#{deploy_to}/gems'
+# }
+
+# set :tmp_dir, '/home/dakotaleedev/tmp'
+
+# namespace :deploy do 
+#   desc 'Restart application'
+#     task :restart do 
+#       on roles(:app), in: :sequence, wait: 5 do 
+#         capture("#{deploy_to}/bin/restart")
+#     end
+#   end
+# end
+
+# namespace :gems do 
+#   task :bundle do
+#     run "cd #{release_path} && bundle install --deployment --without development test"
+#   end
+# end
+
+# after 'deploy:publishing', 'gems:bundle'
+# after 'deploy:publishing', 'deploy:restart'
+
+# # Default value for :scm is :git
+# # set :scm, :git
+
+# # Default value for :format is :airbrussh.
+# # set :format, :airbrussh
+
+# # You can configure the Airbrussh format using :format_options.
+# # These are the defaults.
+# # set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+
+# # Default value for :pty is false
+# # set :pty, true
+
+# # Default value for :linked_files is []
+# # append :linked_files, 'config/database.yml', 'config/secrets.yml'
+
+# # Default value for linked_dirs is []
+# # append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+
+# # Default value for default_env is {}
+# # set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# # Default value for keep_releases is 5
+# # set :keep_releases, 5
